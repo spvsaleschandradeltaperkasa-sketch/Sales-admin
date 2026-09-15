f7fbfe439c0d2ea6540f22d19e76a25a5a77a962
spvsaleschandradeltaperkasa-sketch <spvsaleschandradeltaperkasa@gmail.com>
2026-09-15 12:07:50 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
